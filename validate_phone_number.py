9268c51c3124a65b15b82004c4572f4ca1082cdb
Grootcode21 <antonyndichu15@gmail.com>
2025-01-13 01:15:41 +0300

    validate function

---
+++ validate_phone_number.py
@@ -0,0 +1,11 @@
+import phonenumbers as pn 
+#pip install phonenumbers
+
+def phone_validation(phone_number):
+    try:
+        parsed_number = pn.parse(phone_number)
+        if pn.is_valid_number(parsed_number):
+            return pn.format_number(parsed_number, pn.PhoneNumberFormat.E164)
+
+    except pn.NumberParseException as e:
+        return f"Invalid phone number: {str(e)}"
