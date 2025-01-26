f88861fdfd7a221d682eaf90908a5a3d2a82f749
Grootcode21 <antonyndichu15@gmail.com>
2025-01-26 18:21:45 +0300

    fixing typos

--- validate_phone_number.py
+++ validate_phone_number.py
@@ -6,9 +6,9 @@ def phone_validation(phone_number):
     try:
         parsed_number = pn.parse(phone_number)
         if pn.is_valid_number(parsed_number):
-            return f"Valid: {pn.format_number(parsed_number, pn.PhoneNumberFormat.E164)}
+            return f"Valid: {pn.format_number(parsed_number, pn.PhoneNumberFormat.E164)}"
         else:
-        return "Invalid phone number"
+            return "Invalid phone number"
     except pn.NumberParseException:
         return "Invalid phone number"
 
