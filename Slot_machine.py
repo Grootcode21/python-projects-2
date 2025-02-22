30715bd3e1560fbe478e2101ff96454e1a78c778
Grootcode21 <antonyndichu15@gmail.com>
2025-02-22 20:33:30 +0300

    global variables

--- Slot_machine.py
+++ Slot_machine.py
@@ -1,6 +1,20 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
-def deposit():
+def get_number_of_lines():
     while True:
-        amount - input("What amount would you like to deposit? $")
-        if amount.isdigit():
-            amount = int(amount)
+        lines = input("Enter the no of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES 0:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines  
+
+def main():
+    balance = deposit()  
