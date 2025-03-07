797f01b1c9195662dc27ab456335591599755554
Grootcode21 <antonyndichu15@gmail.com>
2025-03-07 16:31:31 +0300

    password check

--- validate_password.py
+++ validate_password.py
@@ -1,20 +1,19 @@
 password = input("Please enter your password: ")
 
-
 def validate_password():
     if len(password) < 8:
-        return False
+        print("Your password is too short")
     elif not any(char.isupper() for char in password):
-         return False
+         print("Your password does not have an upper case")
     elif not any(char.islower() for char in password):
-        return False
+        print("Your password does not have a lower case")
     elif not any(char.isdigit() for char in password):
-        return False
+        print("Your password does not have a digit")
     elif not any(char in "!@#$%^&*" for char in password):
-        return False
+        print("Your password does not have special character-s")
     elif " " in password:
-        return False
+        print("Your password contains spaces")
     else:
-        return True
-    
+        print("Your password is password")
+
 validate_password()
