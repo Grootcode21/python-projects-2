879bbe3990602f1f92ab0e95449ca7a2ba3da43e
Grootcode21 <antonyndichu15@gmail.com>
2025-03-14 14:05:38 +0300

    racers range

--- turtle_race.py
+++ turtle_race.py
@@ -1,3 +1,7 @@
+import turtle
+
+
+
 def get_number_of_racers():
     racers = 0
     while True:
@@ -5,6 +9,14 @@ def get_number_of_racers():
         if racers.isdigit():
             racers = int(racers)
         else:
-            print("Invalid. Try Again!")
+            print("Input not numeric. Try Again!")
             continue
+        if 2 <= racers <= 10:
+            return racers
+        else:
+            print("The number is not in ranger 2-10. Try Again!")
 
+racers = get_number_of_racers()
+print(racers)
+
+ 
