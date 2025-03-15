a7ff0dc98eb2049c78104a0e0b6e1d0439a2734b
Grootcode21 <antonyndichu15@gmail.com>
2025-03-15 21:52:46 +0300

    turtle functionality

--- turtle_race.py
+++ turtle_race.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 WIDTH, HEIGHT = 500, 500
 
@@ -25,6 +26,12 @@ def init_turtle():
     screen.title("Turtle Racing!")
     
 racers = get_number_of_racers()
-print(racers)
+init_turtle()
 
- 
+racer = turtle.Turtle()
+racer.forward(100)
+racer.left(90)
+racer.forward(100)
+racer.left(90)
+racer.backward(100)
+time.sleep(25)
