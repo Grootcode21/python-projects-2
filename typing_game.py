d2357f1300bb3257d47f673c7d2745eae668730a
Grootcode21 <antonyndichu15@gmail.com>
2025-03-22 16:45:40 +0300

    customize terminal

--- typing_game.py
+++ typing_game.py
@@ -4,8 +4,13 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
     stdscr.clear()
-    stdscr.addstr("Hello World")
+    #stdscr.addstr("Hello World", curses.color_pair(2))
+    stdscr.addstr(1,0, "Hello World")
     stdscr.refresh()
     stdscr.getkey()
 
