87cc61d86d0c3cbe17d228570f74afae00ee2fc0
Grootcode21 <antonyndichu15@gmail.com>
2025-03-22 16:17:04 +0300

    how to run

--- typing_game.py
+++ typing_game.py
@@ -1,4 +1,5 @@
 #pip install curses
+#To run: nagivate to where the file is,click at the top C:\Users\ALIENWARE\Desktop\SOURCE CODES\Python Projects 2, type cmd click Enter
 import curses
 from curses import wrapper
 
