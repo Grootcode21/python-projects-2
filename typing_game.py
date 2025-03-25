be3a8020536f8f7c17040bfe18a4b9e3bc1d7d22
Grootcode21 <antonyndichu15@gmail.com>
2025-03-25 17:48:45 +0300

    initiate screen

--- typing_game.py
+++ typing_game.py
@@ -3,6 +3,10 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.refresh()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_WHITE)
