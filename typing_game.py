7935631d3e78fa9f8a3798f43adf1db87d27ee0f
Grootcode21 <antonyndichu15@gmail.com>
2025-03-26 17:13:30 +0300

    optimize screen

--- typing_game.py
+++ typing_game.py
@@ -6,9 +6,20 @@ from curses import wrapper
 def start_screen(stdscr):
     stdscr.clear()
     stdscr.addstr("Welcome to the Speed Typing Test")
-    stdscr.sddstr("\nPress any key to begin")
+    stdscr.stdstr("\nPress any key to begin")
     stdscr.refresh()
     stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello World, this is a nice way to learn typing"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
@@ -16,5 +27,6 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
     stdscr.screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
