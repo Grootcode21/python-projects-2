41d028c18a7cfd18f7495824133ab9c783614ecb
Grootcode21 <antonyndichu15@gmail.com>
2025-03-31 09:15:48 +0300

    choices

--- ATM_simulator.py
+++ ATM_simulator.py
@@ -1,2 +1,10 @@
 balance = 1000000
 
+while True:
+    print(" Welcome ")
+    print("1. Check balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Exit")
+
+    choice = int(input("Enter your choice: "))
