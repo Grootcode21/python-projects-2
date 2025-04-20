505829d165c2d1288aec50a33d569334230aa1c8
Grootcode21 <antonyndichu15@gmail.com>
2025-04-20 23:44:14 +0300

    alarm display

--- alarm_clock.py
+++ alarm_clock.py
@@ -4,9 +4,13 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN  = "\033[H"
+
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -15,6 +19,6 @@ def alarm(seconds):
         minutes_left = time_left //60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left}:{seconds_left}")
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
 
         
