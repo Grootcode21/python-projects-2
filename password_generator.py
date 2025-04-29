6d28d4cc1605f75444bc11de2af4d17933fa45c8
Grootcode21 <antonyndichu15@gmail.com>
2025-04-29 17:42:36 +0300

    generate pswd function

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,11 @@
 import random 
 import string
 
+def generate_password(min_lenght, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    print(letters, special, digits)
+
+generate_password(10)
