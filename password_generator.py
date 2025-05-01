43e17bfbadd1ca50be96176f2bcd8b69bc5a222c
Grootcode21 <antonyndichu15@gmail.com>
2025-05-01 17:51:21 +0300

    criteria check

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,7 @@
 import random 
 import string
 
-def generate_password(min_lenght, numbers=True, special_characters=True):
+def generate_password(min_length, numbers=True, special_characters=True):
     letters = string.ascii_letters
     digits = string.digits
     special = string.punctuation
@@ -16,5 +16,14 @@ def generate_password(min_lenght, numbers=True, special_characters=True):
     meets_criteria = False
     has_number = False
     has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        if new_char in special:
+            has_special = True
 
 generate_password(10)
