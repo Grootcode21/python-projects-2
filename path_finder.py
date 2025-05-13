b26c4ed138999dce735bb95b477275833bb691e4
Grootcode21 <antonyndichu15@gmail.com>
2025-05-13 17:46:09 +0300

    std color pair

--- path_finder.py
+++ path_finder.py
@@ -10,8 +10,13 @@ maze = [
 
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    blue_and_black = curses.color_pair(1)
+
     stdscr.clear()
-    stdscr.addstrs()
+    stdscr.addstrs(0, 0, "Hello World!", blue_and_black)
     stdscr.refresh()
     stdscr.getch()
+
+wrapper(main)
 
