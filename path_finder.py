fdf3eabcf04cedede4d8908f35b8a51720b88f39
Grootcode21 <antonyndichu15@gmail.com>
2025-05-14 17:47:42 +0300

    breadth search algorithm

--- path_finder.py
+++ path_finder.py
@@ -11,12 +11,14 @@ maze = [
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    blue_and_black = curses.color_pair(1)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    
+    #Breadth-search algorithm
 
-    stdscr.clear()
-    stdscr.addstrs(0, 0, "Hello World!", blue_and_black)
-    stdscr.refresh()
-    stdscr.getch()
+    # stdscr.clear()
+    # stdscr.addstrs(0, 0, "Hello World!", blue_and_black)
+    # stdscr.refresh()
+    # stdscr.getch()
 
 wrapper(main)
 
