6eed212091695886d6809b1cf4f78cf63a0b654e
Grootcode21 <antonyndichu15@gmail.com>
2025-06-01 21:56:24 +0300

    get links

--- nba_scores.py
+++ nba_scores.py
@@ -1,3 +1,13 @@
 #pip install requests
 from requests import get
 from pprint import PrettyPrinter
+
+BASE_URL = "https:/data.nba.net"
+ALL_JSON = "prod/v1/today.json"
+
+printer = PrettyPrinter()
+
+def get_links():
+    data = get(BASE_URL + ALL_JSON).json()
+    links = data['links']
+    return links
