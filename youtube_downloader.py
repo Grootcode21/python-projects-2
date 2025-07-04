b302443c9291fd41dbcbe85039361e050db098a3
Grootcode21 <antonyndichu15@gmail.com>
2025-07-04 21:40:57 +0300

    pytube and imports

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,1 +1,12 @@
 #pip install pytube
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        
+
+    except Exception as e:
+        print(e)
