a184d965c1ed0bf0a3726a1911218aaa221906cc
Grootcode21 <antonyndichu15@gmail.com>
2025-07-06 22:53:44 +0300

    resolution

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -6,7 +6,8 @@ from tkinter import filedialog
 def download_video(url, save_path):
     try:
         yt = YouTube(url)
-        
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        highest_res_stream = streams.get_highest_resolution()
 
     except Exception as e:
         print(e)
