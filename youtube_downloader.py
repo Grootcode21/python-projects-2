7460e2bf267f56522a915646476df2c8d64e40e5
Grootcode21 <antonyndichu15@gmail.com>
2025-07-07 23:34:52 +0300

    resolution

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -8,6 +8,9 @@ def download_video(url, save_path):
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension="mp4")
         highest_res_stream = streams.get_highest_resolution()
-
+        highest_res_stream.download(output_path=save_path)
+        print("Video downloaded successfully")
     except Exception as e:
         print(e)
+
+url = ""
