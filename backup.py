d60586895a48c8435735da1266d77862a2cd7c29
Grootcode21 <antonyndichu15@gmail.com>
2025-07-17 17:10:24 +0300

    catch duplicate

--- backup.py
+++ backup.py
@@ -7,3 +7,14 @@ import time
 
 source_dir = ""
 destination_dir = ""
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shuttl.copytree(source, dest_dir)
+        print(f"Folder copied to {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in {dest}")
+        
