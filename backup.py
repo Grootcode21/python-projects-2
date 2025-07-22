8cbf23c0502f00dd8870f836063e6c9eb1c2dd1e
Grootcode21 <antonyndichu15@gmail.com>
2025-07-22 23:06:19 +0300

    backup schedule

--- backup.py
+++ backup.py
@@ -18,7 +18,12 @@ def copy_folder_to_directory(source, dest):
     except FileExistsError:
         print(f"Folder already exists in {dest}")
 
-def 1():
-    copy_folder_to_directory(source_dir, destination_dir)
 
-schedule.every().day.at("6:55").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+schedule.every().day.at("18:55").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+#to run, python backup.py
+#to quit, control + C
