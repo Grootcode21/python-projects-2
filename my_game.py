1b968aa3661c2e816f69a9c144dd6a3428095685
Grootcode21 <antonyndichu15@gmail.com>
2025-08-01 17:36:55 +0300

    fixing typos

--- my_game.py
+++ my_game.py
@@ -14,15 +14,17 @@ def generate_code():
     return code
 
 def guess_code():
-    guess = input("Guess: ").upper().split(" ")
+    while True:
+        guess = input("Guess: ").upper().split(" ")
 
-    if len(guess)!= 4:
-        print(f"You must guess {CODE_LENGTH} colors.")
-        continue
+        if len(guess)!= CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors.")
+            continue
 
-    if color in guess:
-        if color not in COLORS:
-            print(f"Invalid color: {color}. Try Again")
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid color: {color}. Try Again")
+                break
+        else:
             break
-    else:
-        break
+
