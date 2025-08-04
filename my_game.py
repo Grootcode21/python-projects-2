2e9da8cc25b3133406bbdb0be291da2ce947bf49
Grootcode21 <antonyndichu15@gmail.com>
2025-08-04 15:49:55 +0300

    real color guess

--- my_game.py
+++ my_game.py
@@ -30,3 +30,16 @@ def guess_code():
 
 def check_code(guess, real_code):
     color_counts = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_counts:
+            color_counts[color] = 0
+        color_counts[color] +=1
+
+    for guess_color, real_color in zip(guess, real_code):
+        if guess_color ==  real_color:
+            correct_pos +=1
+            color_counts[guess_color] -= 1 
+             
