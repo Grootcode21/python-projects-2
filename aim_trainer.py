93832398436afacde1a18c48f80a08793a190bc1
Grootcode21 <antonyndichu15@gmail.com>
2025-08-15 00:47:22 +0300

    main function

--- aim_trainer.py
+++ aim_trainer.py
@@ -8,3 +8,13 @@ import pygame
 WIDTH, HEIGHT = 800, 600
 
 WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption("Aim Trainer")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pyge.QUIT:
+                run = False
+                break
